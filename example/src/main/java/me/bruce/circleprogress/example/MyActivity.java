b4c2720accd2e5e598a34bd34feeee7d929e8605
Bruce Lee <bruceinpeking@gmail.com>
2014-10-31 14:50:50 +0800

    add customer attributes support

--- example/src/main/java/me/bruce/circleprogress/example/MyActivity.java
+++ example/src/main/java/me/bruce/circleprogress/example/MyActivity.java
@@ -5,12 +5,39 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.bruce.circleprogress.CircleView;
+
 
 public class MyActivity extends ActionBarActivity {
+    private Timer timer;
+    private CircleView circleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        circleView = (CircleView) findViewById(R.id.circleview);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        circleView.setProgress(circleView.getProgress() + 1);
+                    }
+                });
+            }
+        }, 1000, 100);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
     }
 }
--- library/src/main/java/me/bruce/circleprogress/CircleView.java
+++ library/src/main/java/me/bruce/circleprogress/CircleView.java
@@ -1,10 +1,13 @@
 package me.bruce.circleprogress;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -13,11 +16,48 @@ import android.view.View;
  */
 public class CircleView extends View {
     private Paint finishedPaint;
-    private Paint innerPaint;
+    private Paint unfinishedPaint;
+    private Paint innerCirclePaint;
     private Paint textPaint;
-    private int strokeWidth = 20;
-    private int textSize = 38;
-    private int progress = 80;
+
+    private float textSize;
+    private int progress = 0;
+    private int max = 100;
+    private boolean showText = true;
+    public enum ProgressTextVisibility{
+        Visible,Invisible
+    };
+    private static final int PROGRESS_TEXT_VISIBLE = 0;
+    private static final int PROGRESS_TEXT_INVISIBLE = 1;
+
+    private int finishedStrokeColor;
+    private int unfinishedStrokeColor;
+    private float finishedStrokeWidth;
+    private float unfinishedStrokeWidth;
+    private int textColor;
+
+    private final int default_finished_color = Color.GREEN;
+    private final int default_unfinished_color = Color.LTGRAY;
+    private final int default_text_color = Color.BLACK;
+    private final float default_stroke_width;
+    private final float default_text_size;
+    private final int min_size;
+
+    private String prefixText = "";
+    private String suffixText = "%";
+
+    private static final String INSTANCE_STATE = "saved_instance";
+    private static final String INSTANCE_TEXT_COLOR = "text_color";
+    private static final String INSTANCE_TEXT_SIZE = "text_size";
+    private static final String INSTANCE_FINISHED_STROKE_WIDTH = "finished_stroke_width";
+    private static final String INSTANCE_FINISHED_STROKE_COLOR = "finished_stroke_color";
+    private static final String INSTANCE_UNFINISHED_STROKE_WIDTH = "unfinished_stroke_width";
+    private static final String INSTANCE_UNFINISHED_STROKE_COLOR = "unfinished_stroke_color";
+    private static final String INSTANCE_MAX = "max";
+    private static final String INSTANCE_PROGRESS = "progress";
+    private static final String INSTANCE_SUFFIX = "suffix";
+    private static final String INSTANCE_PREFIX = "prefix";
+    private static final String INSTANCE_SHOW_TEXT = "show_text";
 
     public CircleView(Context context) {
         this(context, null);
@@ -29,21 +69,51 @@ public class CircleView extends View {
 
     public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+
+        default_stroke_width = dp2px(10);
+        default_text_size = sp2px(18);
+        min_size = (int) dp2px(100);
+
+        final TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CircleProgress, defStyleAttr, 0);
+
+        finishedStrokeColor = attributes.getColor(R.styleable.CircleProgress_progress_finished_color, default_finished_color);
+        unfinishedStrokeColor = attributes.getColor(R.styleable.CircleProgress_progress_unfinished_color, default_unfinished_color);
+        textColor = attributes.getColor(R.styleable.CircleProgress_progress_text_color, default_text_color);
+        textSize = attributes.getDimension(R.styleable.CircleProgress_progress_text_size, default_text_size);
+
+        finishedStrokeWidth = attributes.getDimension(R.styleable.CircleProgress_progress_finished_stroke_width, default_stroke_width);
+        unfinishedStrokeWidth = attributes.getDimension(R.styleable.CircleProgress_progress_unfinished_stroke_width, default_stroke_width);
+
+        int textVisible = attributes.getInt(R.styleable.CircleProgress_progress_text_visibility, PROGRESS_TEXT_VISIBLE);
+        if(textVisible != PROGRESS_TEXT_VISIBLE){
+            showText = false;
+        }
+
+        setProgress(attributes.getInt(R.styleable.CircleProgress_progress, 0));
+        setMax(attributes.getInt(R.styleable.CircleProgress_max, 100));
+        attributes.recycle();
+
+        initPaints();
     }
 
-    private void init() {
+    private void initPaints() {
         finishedPaint = new Paint();
-        finishedPaint.setColor(Color.GREEN);
+        finishedPaint.setColor(finishedStrokeColor);
         finishedPaint.setStyle(Paint.Style.STROKE);
         finishedPaint.setAntiAlias(true);
-        finishedPaint.setStrokeWidth(strokeWidth);
+        finishedPaint.setStrokeWidth(finishedStrokeWidth);
 
-        innerPaint = new Paint();
-        innerPaint.setColor(Color.WHITE);
+        unfinishedPaint = new Paint();
+        unfinishedPaint.setColor(unfinishedStrokeColor);
+        unfinishedPaint.setStyle(Paint.Style.STROKE);
+        unfinishedPaint.setAntiAlias(true);
+        unfinishedPaint.setStrokeWidth(unfinishedStrokeWidth);
+
+        innerCirclePaint = new Paint();
+        innerCirclePaint.setColor(Color.WHITE);
 
         textPaint = new Paint();
-        textPaint.setColor(Color.BLACK);
+        textPaint.setColor(textColor);
         textPaint.setTextSize(textSize);
     }
 
@@ -53,21 +123,159 @@ public class CircleView extends View {
 
     public void setProgress(int progress) {
         this.progress = progress;
+        if (this.progress > getMax()) {
+            this.progress %= getMax();
+        }
+        invalidate();
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        if (max > 0) {
+            this.max = max;
+            invalidate();
+        }
+    }
+
+    public float getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(float textSize) {
+        this.textSize = textSize;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public int getFinishedStrokeColor() {
+        return finishedStrokeColor;
+    }
+
+    public int getUnfinishedStrokeColor() {
+        return unfinishedStrokeColor;
+    }
+
+    public float getFinishedStrokeWidth() {
+        return finishedStrokeWidth;
+    }
+
+    public float getUnfinishedStrokeWidth() {
+        return unfinishedStrokeWidth;
+    }
+
+    public String getPrefixText() {
+        return prefixText;
+    }
+
+    public String getSuffixText() {
+        return suffixText;
     }
 
     private float getProgressAngle() {
-        return getProgress() * 3.6f;
+        return getProgress() / (float) max * 360f;
+    }
+
+    @Override
+    protected int getSuggestedMinimumHeight() {
+        return min_size;
+    }
+
+    @Override
+    protected int getSuggestedMinimumWidth() {
+        return min_size;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measure(widthMeasureSpec), measure(heightMeasureSpec));
+    }
+
+    private int measure(int measureSpec){
+        int result;
+        int mode = MeasureSpec.getMode(measureSpec);
+        int size = MeasureSpec.getSize(measureSpec);
+        if(mode == MeasureSpec.EXACTLY){
+            result = size;
+        }else{
+            result = min_size;
+            if(mode == MeasureSpec.AT_MOST){
+                result = Math.min(result, size);
+            }
+        }
+        return result;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        RectF outerRect = new RectF(getLeft() + strokeWidth / 2, getTop() + strokeWidth / 2, getRight() - strokeWidth / 2, getBottom() - strokeWidth / 2);
+        RectF outerRect = new RectF(getLeft() + finishedStrokeWidth / 2, getTop() + finishedStrokeWidth / 2, getRight() - finishedStrokeWidth / 2, getBottom() - finishedStrokeWidth / 2);
         canvas.drawArc(outerRect, 0, getProgressAngle(), false, finishedPaint);
-        canvas.drawCircle(getWidth() / 2.0f, getHeight() / 2.0f, (getWidth() - strokeWidth) / 2.0f, innerPaint);
-        String text = progress + "%";
-        float textHeight = textPaint.descent() + textPaint.ascent();
+        canvas.drawArc(outerRect, getProgressAngle(), 360 - getProgressAngle(), false, unfinishedPaint);
+        canvas.drawCircle(getWidth() / 2.0f, getHeight() / 2.0f, (getWidth() - finishedStrokeWidth) / 2.0f, innerCirclePaint);
 
-        canvas.drawText(text, (getWidth() - textPaint.measureText(text)) / 2.0f, (getWidth() - textHeight) / 2.0f, textPaint);
+        if (showText) {
+            String text = prefixText + progress + suffixText;
+            float textHeight = textPaint.descent() + textPaint.ascent();
+            canvas.drawText(text, (getWidth() - textPaint.measureText(text)) / 2.0f, (getWidth() - textHeight) / 2.0f, textPaint);
+        }
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(INSTANCE_STATE, super.onSaveInstanceState());
+        bundle.putInt(INSTANCE_TEXT_COLOR, getTextColor());
+        bundle.putFloat(INSTANCE_TEXT_SIZE, getTextSize());
+        bundle.putFloat(INSTANCE_FINISHED_STROKE_WIDTH, getFinishedStrokeWidth());
+        bundle.putFloat(INSTANCE_UNFINISHED_STROKE_WIDTH, getUnfinishedStrokeWidth());
+        bundle.putInt(INSTANCE_FINISHED_STROKE_COLOR, getFinishedStrokeColor());
+        bundle.putInt(INSTANCE_UNFINISHED_STROKE_COLOR, getUnfinishedStrokeColor());
+        bundle.putInt(INSTANCE_MAX, getMax());
+        bundle.putInt(INSTANCE_PROGRESS, getProgress());
+        bundle.putString(INSTANCE_SUFFIX, getSuffixText());
+        bundle.putString(INSTANCE_PREFIX, getPrefixText());
+        bundle.putBoolean(INSTANCE_SHOW_TEXT, showText);
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if(state instanceof Bundle) {
+            final Bundle bundle = (Bundle) state;
+            textColor = bundle.getInt(INSTANCE_TEXT_COLOR);
+            textSize = bundle.getFloat(INSTANCE_TEXT_SIZE);
+            finishedStrokeWidth = bundle.getFloat(INSTANCE_FINISHED_STROKE_WIDTH);
+            unfinishedStrokeWidth = bundle.getFloat(INSTANCE_UNFINISHED_STROKE_WIDTH);
+            finishedStrokeColor = bundle.getInt(INSTANCE_FINISHED_STROKE_COLOR);
+            unfinishedStrokeColor = bundle.getInt(INSTANCE_UNFINISHED_STROKE_COLOR);
+            initPaints();
+            setMax(bundle.getInt(INSTANCE_MAX));
+            setProgress(bundle.getInt(INSTANCE_PROGRESS));
+            prefixText = bundle.getString(INSTANCE_PREFIX);
+            suffixText = bundle.getString(INSTANCE_SUFFIX);
+            showText = bundle.getBoolean(INSTANCE_SHOW_TEXT);
+            super.onRestoreInstanceState(bundle.getParcelable(INSTANCE_STATE));
+            return;
+        }
+        super.onRestoreInstanceState(state);
+    }
+
+    public float dp2px(float dp) {
+        final float scale = getResources().getDisplayMetrics().density;
+        return  dp * scale + 0.5f;
+    }
+
+    public float sp2px(float sp){
+        final float scale = getResources().getDisplayMetrics().scaledDensity;
+        return sp * scale;
     }
 }
