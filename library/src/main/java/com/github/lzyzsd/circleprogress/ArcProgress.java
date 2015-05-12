c762b0ad6d02c57b2c36e8dd88026e9d770bff61
Vivek Kandheri Manohar <vmanohar@visa.com>
2015-05-11 18:01:53 -0700

    Bug fix for arcAngle. Changed getDimension to getFloat

--- library/src/main/java/com/github/lzyzsd/circleprogress/ArcProgress.java
+++ library/src/main/java/com/github/lzyzsd/circleprogress/ArcProgress.java
@@ -98,7 +98,7 @@ public class ArcProgress extends View {
         unfinishedStrokeColor = attributes.getColor(R.styleable.ArcProgress_arc_unfinished_color, default_unfinished_color);
         textColor = attributes.getColor(R.styleable.ArcProgress_arc_text_color, default_text_color);
         textSize = attributes.getDimension(R.styleable.ArcProgress_arc_text_size, default_text_size);
-        arcAngle = attributes.getDimension(R.styleable.ArcProgress_arc_angle, default_arc_angle);
+        arcAngle = attributes.getFloat(R.styleable.ArcProgress_arc_angle, default_arc_angle);
         setMax(attributes.getInt(R.styleable.ArcProgress_arc_max, default_max));
         setProgress(attributes.getInt(R.styleable.ArcProgress_arc_progress, 0));
         strokeWidth = attributes.getDimension(R.styleable.ArcProgress_arc_stroke_width, default_stroke_width);
