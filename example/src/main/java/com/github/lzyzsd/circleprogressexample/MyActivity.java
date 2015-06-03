81f6b60a0ede70be456f66b1f5777e95300db6d6
javierjmc <javier.jmc@gmail.com>
2015-06-03 20:42:10 +0200

    Added animation in donutProgress

--- example/src/main/java/com/github/lzyzsd/circleprogressexample/MyActivity.java
+++ example/src/main/java/com/github/lzyzsd/circleprogressexample/MyActivity.java
@@ -1,17 +1,23 @@
 package com.github.lzyzsd.circleprogressexample;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import android.view.animation.DecelerateInterpolator;
 
 import com.github.lzyzsd.circleprogress.ArcProgress;
 import com.github.lzyzsd.circleprogress.CircleProgress;
 import com.github.lzyzsd.circleprogress.DonutProgress;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyActivity extends ActionBarActivity {
     private Timer timer;
@@ -19,13 +25,27 @@ public class MyActivity extends ActionBarActivity {
     private CircleProgress circleProgress;
     private ArcProgress arcProgress;
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
         donutProgress = (DonutProgress) findViewById(R.id.donut_progress);
-        circleProgress = (CircleProgress) findViewById(R.id.circle_progress);
-        arcProgress = (ArcProgress) findViewById(R.id.arc_progress);
+
+        /*AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.progress_anim2);
+        set.setTarget(donutProgress);
+        set.start();*/
+
+        /*ValueAnimator animation = ValueAnimator.ofObject(new IntEvaluator(), 0, 100);
+        animation.setDuration(1000);
+        animation.start();*/
+
+        /*PropertyValuesHolder donutAlphaProperty = PropertyValuesHolder.ofFloat("alpha", 0f, 1f);
+        PropertyValuesHolder donutProgressProperty = PropertyValuesHolder.ofInt("donut_progress", 0, 100);
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(donutProgress, donutAlphaProperty, donutProgressProperty);
+        animator.setDuration(2000);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.start();*/
 
         timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -34,13 +54,22 @@ public class MyActivity extends ActionBarActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        donutProgress.setProgress(donutProgress.getProgress() + 1);
-                        circleProgress.setProgress(circleProgress.getProgress() + 1);
-                        arcProgress.setProgress(arcProgress.getProgress() + 1);
+                        boolean a = false;
+                        if (a) {
+                            ObjectAnimator anim = ObjectAnimator.ofInt(donutProgress, "progress", 0, 10);
+                            anim.setInterpolator(new DecelerateInterpolator());
+                            anim.setDuration(500);
+                            anim.start();
+                        } else {
+                            AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(MyActivity.this, R.animator.progress_anim);
+                            set.setInterpolator(new DecelerateInterpolator());
+                            set.setTarget(donutProgress);
+                            set.start();
+                        }
                     }
                 });
             }
-        }, 1000, 100);
+        }, 0, 2000);
     }
 
     @Override
@@ -48,6 +77,7 @@ public class MyActivity extends ActionBarActivity {
         super.onDestroy();
         timer.cancel();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
