dc3f8fd2260ab68054b10575ee61656b0d62a1c6
DarzuL <guillaumebourderye@gmail.com>
2016-06-04 00:56:36 +0200

    Made Utils final and add private constructor
    An Utils class shouldn't be able to be instantiated and/or extended

--- library/src/main/java/com/github/lzyzsd/circleprogress/Utils.java
+++ library/src/main/java/com/github/lzyzsd/circleprogress/Utils.java
@@ -5,7 +5,11 @@ import android.content.res.Resources;
 /**
  * Created by bruce on 14-11-6.
  */
-public class Utils {
+public final class Utils {
+    
+    private Utils() {
+    }
+    
     public static float dp2px(Resources resources, float dp) {
         final float scale = resources.getDisplayMetrics().density;
         return  dp * scale + 0.5f;
